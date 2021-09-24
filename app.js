f14c383f20ca9113039a1f0fcaf26a0d12304aeb
Jordan Selig <11475594+seligj95@users.noreply.github.com>
2021-09-24 16:40:56 -0400

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const port = 2000;
 
 
 // create connection to database
-the mysql.createConnection function takes in a configuration object which contains host, user, password and the database name.
+// the mysql.createConnection function takes in a configuration object which contains host, user, password and the database name.
 const db = mysql.createConnection ({
     host: 'localhost',
     user: 'root',
